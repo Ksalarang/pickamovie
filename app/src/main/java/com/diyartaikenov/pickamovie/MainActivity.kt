418f14bb92ef8ab6ee612c7711e6d751dabcabed
ksalarang <anbleidd@gmail.com>
2022-04-25 14:45:25 +0600

    setup navigation and first two fragments

--- app/src/main/java/com/diyartaikenov/pickamovie/MainActivity.kt
+++ app/src/main/java/com/diyartaikenov/pickamovie/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.diyartaikenov.pickamovie
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.diyartaikenov.pickamovie.databinding.ActivityMainBinding
 
+/**
+ * A Main activity that hosts all Fragments for this app and hosts the nav controller.
+ */
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = findNavController(R.id.nav_host_fragment)
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/diyartaikenov/pickamovie/ui/SeriesFragment.kt
@@ -0,0 +1,23 @@
+package com.diyartaikenov.pickamovie.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.diyartaikenov.pickamovie.databinding.FragmentSeriesBinding
+
+class SeriesFragment : Fragment() {
+
+    private var _binding: FragmentSeriesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSeriesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
