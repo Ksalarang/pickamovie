d1c426366a0fc02f2b597d513694e99cf01b53db
ksalarang <anbleidd@gmail.com>
2022-04-28 04:32:39 +0600

    setup Hilt library

--- app/src/main/java/com/diyartaikenov/pickamovie/ui/MyListsFragment.kt
+++ app/src/main/java/com/diyartaikenov/pickamovie/ui/MyListsFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.diyartaikenov.pickamovie.databinding.FragmentMyListsBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MyListsFragment: Fragment() {
 
     private var _binding: FragmentMyListsBinding? = null
