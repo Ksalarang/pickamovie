c3cf536ea24fd32ce6518d1242b742db091b2b7d
ksalarang <anbleidd@gmail.com>
2022-04-29 12:53:42 +0600

    add docs to various classes

--- app/src/main/java/com/diyartaikenov/pickamovie/database/DatabaseMovie.kt
+++ app/src/main/java/com/diyartaikenov/pickamovie/database/DatabaseMovie.kt
@@ -1,26 +1,38 @@
 package com.diyartaikenov.pickamovie.database
 
+import androidx.room.PrimaryKey
 import com.diyartaikenov.pickamovie.model.Genre
 import com.squareup.moshi.Json
 
 data class DatabaseMovie(
-    val adult: Boolean,
+    @PrimaryKey
+    val id: Long,
+    val title: String,
+    val overview: String? = null,
+    val genres: List<Genre>,
+
+    @Json(name = "release_date")
+    val releaseDate: String,
+
+    /**
+     * Counted in minutes
+     */
+    val runtime: Int? = null,
+
+    @Json(name = "poster_path")
+    val posterPath: String?,
     @Json(name = "backdrop_path")
     val backdropPath: String?,
-    @Json(name = "belongs_to_collection")
-    val belongsToCollection: Any?,
-    val budget: Int,
-    val genres: List<Genre>,
-    val homepage: String?,
-    val id: Long,
+
     @Json(name = "imdb_id")
     val imdbId: String?,
+
     @Json(name = "original_language")
     val originalLanguage: String,
     @Json(name = "original_title")
     val originalTitle: String,
-    val overview: String,
+
     val popularity: Double,
-    @Json(name = "poster_path")
-    val posterPath: String?,
+
+    val hasVideo: Boolean,
 )
--- app/src/main/java/com/diyartaikenov/pickamovie/network/DataTransferObjects.kt
+++ app/src/main/java/com/diyartaikenov/pickamovie/network/DataTransferObjects.kt
@@ -4,6 +4,9 @@ import com.diyartaikenov.pickamovie.model.Movie
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
+/**
+ * 200 code response object that contains a list of popular movies.
+ */
 @JsonClass(generateAdapter = true)
 data class PopularMovieContainer(
     val page: Int,
@@ -15,35 +18,29 @@ data class PopularMovieContainer(
     val totalPages: Int,
 )
 
+/**
+ * Movie class retrieved with popular movies query.
+ */
 @JsonClass(generateAdapter = true)
 data class PopularMovie(
     val id: Long,
     val title: String,
     val overview: String,
-    @Json(name = "genre_ids")
-    val genreIds: List<Int>,
-    @Json(name = "release_date")
-    val releaseDate: String,
 
     @Json(name = "poster_path")
     val posterPath: String?,
-    @Json(name = "backdrop_path")
-    val backdropPath: String?,
+    @Json(name = "release_date")
+    val releaseDate: String,
 
     val popularity: Double,
+
     @Json(name = "vote_average")
     val voteAverage: Double,
     @Json(name = "vote_count")
     val voteCount: Int,
-
-    @Json(name = "original_title")
-    val originalTitle: String,
-    @Json(name = "original_language")
-    val originalLanguage: String,
-    val video: Boolean,
-    val adult: Boolean,
 )
 
+// todo: remove
 fun PopularMovieContainer.asDomainModel(): List<Movie> {
     return movies.map {
         Movie(
