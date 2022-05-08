68714956777fd80932ab2a175801abe3d69e5fc5
ksalarang <anbleidd@gmail.com>
2022-05-08 22:23:03 +0600

    setup action bar with navController

--- app/src/main/java/com/diyartaikenov/pickamovie/ui/MainActivity.kt
+++ app/src/main/java/com/diyartaikenov/pickamovie/ui/MainActivity.kt
@@ -2,7 +2,11 @@ package com.diyartaikenov.pickamovie.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.diyartaikenov.pickamovie.R
 import com.diyartaikenov.pickamovie.databinding.ActivityMainBinding
@@ -17,14 +21,23 @@ const val TAG = "myTag"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.bottomNavView.setupWithNavController(
-            findNavController(R.id.nav_host_fragment)
-        )
+        navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.bottomNavView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
