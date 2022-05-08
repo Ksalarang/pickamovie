2bce8ffbbb4a5eb398d2d53d3692fef63b9e7702
ksalarang <anbleidd@gmail.com>
2022-05-09 00:58:26 +0600

    revert setup action bar with navController

--- app/src/main/java/com/diyartaikenov/pickamovie/ui/MainActivity.kt
+++ app/src/main/java/com/diyartaikenov/pickamovie/ui/MainActivity.kt
@@ -21,23 +21,14 @@ const val TAG = "myTag"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var navController: NavController
-    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        navController = findNavController(R.id.nav_host_fragment)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        binding.bottomNavView.setupWithNavController(navController)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        binding.bottomNavView.setupWithNavController(
+            findNavController(R.id.nav_host_fragment)
+        )
     }
 }
