c2a09b042e3e528b89bcaca368ed5f75d817125d
ksalarang <anbleidd@gmail.com>
2022-05-10 16:41:02 +0600

    add MovieDetailsFragment

---
+++ app/src/main/java/com/diyartaikenov/pickamovie/ui/homeviewpager/MovieDetailsFragment.kt
@@ -0,0 +1,24 @@
+package com.diyartaikenov.pickamovie.ui.homeviewpager
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.diyartaikenov.pickamovie.R
+import com.diyartaikenov.pickamovie.databinding.FragmentMovieDetailsBinding
+
+class MovieDetailsFragment : Fragment() {
+
+    private var _binding: FragmentMovieDetailsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMovieDetailsBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
