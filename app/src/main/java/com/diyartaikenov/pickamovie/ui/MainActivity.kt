2ffeea3560e7a8be6221c0f4d1883d53ff60735d
ksalarang <anbleidd@gmail.com>
2022-05-16 04:38:22 +0600

    Make status bar overlap with MainActivity
    And set the status bar color transparent, so the Main Activity is
    responsible for drawing the status bar's background.

--- app/src/main/java/com/diyartaikenov/pickamovie/ui/MainActivity.kt
+++ app/src/main/java/com/diyartaikenov/pickamovie/ui/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.diyartaikenov.pickamovie.ui
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+import android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.diyartaikenov.pickamovie.R
@@ -29,6 +35,21 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNavView.setupWithNavController(
             findNavController(R.id.nav_host_fragment)
         )
+
+        window.statusBarColor = Color.TRANSPARENT
+
+        if (Build.VERSION.SDK_INT in 21..29) {
+            // Make this activity's window to be responsible
+            // for drawing the status bar background
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.decorView.systemUiVisibility =
+                SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or SYSTEM_UI_FLAG_LAYOUT_STABLE
+        }
+        else if (Build.VERSION.SDK_INT >= 30) {
+            // Making status bar overlaps with the activity
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+        }
     }
 
     /**
